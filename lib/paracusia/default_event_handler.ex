96a15aed430cabda088cb6ed0eae8c25677d98f1
nroi <nroi@mailbox.org>
2016-09-25 23:14:21 +0200

    Pattern match against %PlayerState{}

--- lib/paracusia/default_event_handler.ex
+++ lib/paracusia/default_event_handler.ex
@@ -1,38 +1,39 @@
 defmodule Paracusia.DefaultEventHandler do
+  alias Paracusia.PlayerState
   use GenEvent
   require Logger
 
-  def handle_event({:playlist_changed, _}, state = nil) do
+  def handle_event({:playlist_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "playlist changed."
     {:ok, state}
   end
 
-  def handle_event({:player_changed, _}, state = nil) do
+  def handle_event({:player_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "player changed."
     {:ok, state}
   end
 
-  def handle_event({:mixer_changed, _}, state = nil) do
+  def handle_event({:mixer_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "mixer changed."
     {:ok, state}
   end
 
-  def handle_event({:options_changed, _}, state = nil) do
+  def handle_event({:options_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "options changed."
     {:ok, state}
   end
 
-  def handle_event({:update_changed, _}, state = nil) do
+  def handle_event({:update_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "database updated."
     {:ok, state}
   end
 
-  def handle_event({:database_changed, _}, state = nil) do
+  def handle_event({:database_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "database changed."
     {:ok, state}
   end
 
-  def handle_event({:outputs_changed, _}, state = nil) do
+  def handle_event({:outputs_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "outputs changed."
     {:ok, state}
   end
