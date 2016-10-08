04703ee63c0a7485278d08ac639e88131104412d
nroi <nroi@mailbox.org>
2016-10-08 22:30:19 +0200

    Add missing handle_event clauses, reorder

--- lib/paracusia/default_event_handler.ex
+++ lib/paracusia/default_event_handler.ex
@@ -3,6 +3,23 @@ defmodule Paracusia.DefaultEventHandler do
   use GenEvent
   require Logger
 
+  # TODO the %PlayerState{} does not contain anything about subscriptions, stickers or messages.
+
+  def handle_event({:database_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "database changed."
+    {:ok, state}
+  end
+
+  def handle_event({:update_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "database updated."
+    {:ok, state}
+  end
+
+  def handle_event({:stored_playlist_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "stored playlists changed."
+    {:ok, state}
+  end
+
   def handle_event({:playlist_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "playlist changed."
     {:ok, state}
@@ -18,28 +35,28 @@ defmodule Paracusia.DefaultEventHandler do
     {:ok, state}
   end
 
-  def handle_event({:options_changed, %PlayerState{}}, state = nil) do
-    _ = Logger.info "options changed."
-    {:ok, state}
-  end
-
-  def handle_event({:update_changed, %PlayerState{}}, state = nil) do
-    _ = Logger.info "database updated."
-    {:ok, state}
-  end
-
-  def handle_event({:database_changed, %PlayerState{}}, state = nil) do
-    _ = Logger.info "database changed."
-    {:ok, state}
-  end
-
   def handle_event({:outputs_changed, %PlayerState{}}, state = nil) do
     _ = Logger.info "outputs changed."
     {:ok, state}
   end
 
-  def handle_event({:stored_playlist_changed, %PlayerState{}}, state = nil) do
-    _ = Logger.info "stored playlists changed."
+  def handle_event({:options_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "options changed."
+    {:ok, state}
+  end
+
+  def handle_event({:sticker_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "sticker changed."
+    {:ok, state}
+  end
+
+  def handle_event({:subscription_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "subscription changed."
+    {:ok, state}
+  end
+
+  def handle_event({:message_changed, %PlayerState{}}, state = nil) do
+    _ = Logger.info "message arrived."
     {:ok, state}
   end
 end
