129544ec380ffe0bb8934208d95b57a622019128
nroi <nroi@mailbox.org>
2016-10-13 22:13:35 +0200

    Add metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -2,35 +2,37 @@ defmodule Paracusia.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :paracusia,
+    [
+     app: :paracusia,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package(),
+     description: "MPD client library"
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {Paracusia, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ex_doc, "~> 0.14", only: :dev},
       {:distillery, "~> 0.10.0"}
+    ]
+  end
+
+  defp package do
+    [
+     maintainers: ["Fabian Muscariello"],
+     licenses: ["MIT"]
+	 # TODO add github URL once it's available.
+     # links: %{"GitHub" => "https://github.com/???",
+     #         "Docs" => "http://???"}]
     ]
   end
 end
